ba169b470692d9120b2e24379cfb1c11822e1779
Swatilukhi29 <swatidl0209@gmail.com>
2024-07-25 14:14:34 +0530

    about and notfound page design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import AppLayout from "./Components/AppLayout";
 import Contact from "./Contact";
+import About from "./About";
+import NotFound from "./Components/AppLayout/NotFound";
 
 function App({ children }) {
   return (
@@ -10,8 +12,11 @@ function App({ children }) {
       <Router>
         <AppLayout>
           <Routes>
+            <Route element={<Home />} path="/" />
             <Route element={<Home />} path="/home" />
             <Route element={<Contact/>} path="/contact" />
+            <Route element={<About />} path="/about" />
+            <Route element={<NotFound />} path="*" />
           </Routes>
         </AppLayout>
       </Router>
--- src/Components/AppLayout/index.js
+++ src/Components/AppLayout/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { useLocation } from "react-router-dom";
 
 function AppLayout({ children }) {
+  
+  const EXCLUDED_PATHS = ['/home', '/contact','/about'];
+  const location = useLocation();
+  const showHeaderFooter = EXCLUDED_PATHS.includes(location.pathname);
+
   return (
     <div className="h-screen">
       <div className="h-full overflow-x-hidden">
-        <Header />
+      {showHeaderFooter && <Header />}
         {children}
-        <Footer />
+        {showHeaderFooter && <Footer />}
       </div>
     </div>
   );
